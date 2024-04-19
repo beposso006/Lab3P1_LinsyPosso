2c19d969ebb32d5e5bc762351b020762c338e7cd
beposso006 <belenposso2006@gmail.com>
2024-04-19 14:02:41 -0600

    Primer commit

---
+++ Lab1P3_LinsyPosso.cpp
@@ -0,0 +1,11 @@
+// Lab1P3_LinsyPosso.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World!\n";
+}
+
+
